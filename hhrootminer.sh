0f8f5165742996631c0e8cd9e01ca026081ea644
hhroot <105219296+735840086@users.noreply.github.com>
2023-07-09 01:50:29 +0800

    更新 hhrootminer.sh

--- hhrootminer.sh
+++ hhrootminer.sh
@@ -97,7 +97,7 @@ install_hhrootminer() {
     mkdir -p ${updatePath}
 
     cd ${updatePath}
-    wget --no-check-certificate https://github.com/735840086/hhrootminer/raw/main/hhrootminer
+    wget --no-check-certificate https://raw.github.com/735840086/hhrootminer/blob/main/hhrootminer
     if [ $? -ne 0 ]; then
         exit -1;
     fi

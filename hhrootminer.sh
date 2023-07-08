50cb9a98469c2656fd0ff9570930c2a4f17bab77
hhroot <105219296+735840086@users.noreply.github.com>
2023-07-09 02:01:57 +0800

    Update hhrootminer.sh

--- hhrootminer.sh
+++ hhrootminer.sh
@@ -97,13 +97,13 @@ install_hhrootminer() {
     mkdir -p ${updatePath}
 
     cd ${updatePath}
-    wget --no-check-certificate https://raw.github.com/735840086/hhrootminer/blob/main/hhrootminer
+    wget --no-check-certificate https://github.com/735840086/hhrootminer/blob/d7b21e1bac4bb862bab33687a255c2be4de8aa37/hhrootminer
     if [ $? -ne 0 ]; then
         exit -1;
     fi
     chmod +x hhrootminer
     
-    wget --no-check-certificate https://raw.github.com/735840086/hhrootminer/blob/version
+    wget --no-check-certificate https://github.com/735840086/hhrootminer/blob/d7b21e1bac4bb862bab33687a255c2be4de8aa37/version
         if [ $? -ne 0 ]; then
         exit -1;
     fi
@@ -131,7 +131,7 @@ update_hhrootminer() {
     mkdir -p ${updatePath}
 
     cd ${updatePath}
-       wget --no-check-certificate https://raw.github.com/735840086/hhrootminer/blob/fd1c6b35ea14b31626ee1adc05cd3976e476815d/version
+       wget --no-check-certificate https://github.com/735840086/hhrootminer/blob/d7b21e1bac4bb862bab33687a255c2be4de8aa37/version
     if [ $? -ne 0 ]; then
         exit -1;
     fi
